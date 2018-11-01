a3664e2a5892c6eaced37cb1c2ce9fb1bf398b34
gary.cao <gary.cao@example.com>
2018-11-01 22:35:54 +0800

    demo ui modify

--- app/src/main/java/com/gary/weatherdemo/view/activity/BaseActivity.java
+++ app/src/main/java/com/gary/weatherdemo/view/activity/BaseActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
  * Created by GaryCao on 2018/10/28.
  */
 public class BaseActivity extends Activity {
-    /*GoF4:模板方法? ：提供共用流程实现*/
     // private AdView mAdView;
 
     @Override
